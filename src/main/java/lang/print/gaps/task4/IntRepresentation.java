80bc63039cb87202666b9433f5b9842928351356
Omurbek <omke_aka@mail.ru>
2022-09-05 20:31:52 +0600

    test1

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int c = 'c' ;
+        int h = 'h';
+        int a = 'a';
+        int r = 'r';
+        System.out.println(c+ " " + h + " " + a + " " + r);
     }
 }
